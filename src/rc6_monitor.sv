9ba97ba2b841efcb0cdb59ab348c5d11cd72b856
SingularityKChen <chency_singularity@163.com>
2018-12-17 01:34:51 +0800

    Add the content of monitor class

--- src/rc6_monitor.sv
+++ src/rc6_monitor.sv
@@ -4,6 +4,20 @@
 //						then send it to the scoreboard
 // FileName	: rc6_monitor.sv
 // Coder    : SingularityKChen
-// Edition	: edit 1
-// Date     : DEC 11/2018
+// Edition	: Edit 2
+// Date     : DEC 14/2018
+// 						DEC 16/2018
 //===========================================
+class rc6_monitor;
+	virtual main_port.tb dut_vf;
+	mailbox #(rc6_stimulus_method) monitor_random_data;
+
+	function mo_new (virtual main_port.tb dut_vf);
+		dut_vf = dut_v;
+	endfunction : mo_new
+
+	function rc6_stimulus_method pin2transaction();
+		rc6_stimulus_method transcation = new;
+		transcation.rc6_dataout = dut_vf.data_out;
+	endfunction : 
+endclass : rc6_monitor  
