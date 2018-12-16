cc914bfee1a07ec8cd102c605e9a6bdf864056c7
SingularityKChen <chency_singularity@163.com>
2018-12-17 01:25:50 +0800

    Update rc6_driver.sv
    1. Change the name of virtual interface from tc6_dut_pins_if to main_port.tb;
    2. Delet the useless variable id & id_i;
    3. Correct the sub signal from virtual interface from dut_vf.infa_datain to dut_vf.data_in;

--- src/rc6_driver.sv
+++ src/rc6_driver.sv
@@ -4,14 +4,30 @@
 //						and then send it to DUT
 // FileName	: rc6_driver.sv
 // Coder    : SingularityKChen
-// Edition	: edit 1
+// Edition	: Edit 3
 // Date     : DEC 11/2018
+//						DEC 14/2018
+//						DEC 16/2018
 //===========================================
 class rc6_driver;
+	virtual main_port.tb dut_vf;
 	mailbox #(rc6_stimulus_method) get_random_data;
 	rc6_stimulus_method transaction;
 
-	task run();
-	
+	function dr_new (virtual main_port.tb dut_v);
+		dut_vf = dut_v;
+	endfunction : dr_new
+
+	task run;
+		dut_vf.data_in = 0;
+		@(negedge dut_vf.rset)
+		forever @(posedge dut_vf.clk)
+			if(get_random_data.try_get(transaction)) begin
+				dut_vf.data_in = 0;
+				$display("time:% t % m driver get a stimulus: % h",$time(), transaction.rc6_datain);
+				dut_vf.data_in = transaction.rc6_datain;
+				@(posedge dut_vf.clk);
+				dut_vf.data_in = 0;
+			end
 	endtask : run
-endclass : rc6_driver1
+endclass : rc6_driver	
